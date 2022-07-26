e590de15fd619035aa693608aa3f790eaca5e1df
Partha <ytiamahtrap@gmail.com>
2022-07-26 17:42:34 +0530

    FIX corner movements

--- app.js
+++ app.js
@@ -2,12 +2,109 @@
 
 const snakeBoxes = document.querySelectorAll('main div');
 const canvasBoxNumber = 20;
+const cornerBoxMapping = {
+    up: {
+        0: 380,
+        1: 381,
+        2: 382,
+        3: 383,
+        4: 384,
+        5: 385,
+        6: 386,
+        7: 387,
+        8: 388,
+        9: 389,
+        10: 390,
+        11: 391,
+        12: 392,
+        13: 393,
+        14: 394,
+        15: 395,
+        16: 396,
+        17: 397,
+        18: 398,
+        19: 399,
+        20: 400,
+    },
+    down: {
+        380: 0,
+        381: 1,
+        382: 2,
+        383: 3,
+        384: 4,
+        385: 5,
+        386: 6,
+        387: 7,
+        388: 8,
+        389: 9,
+        390: 10,
+        391: 11,
+        392: 12,
+        393: 13,
+        394: 14,
+        395: 15,
+        396: 16,
+        397: 17,
+        398: 18,
+        399: 19,
+        400: 20,
+    },
+    left: {
+        0: 19,
+        20: 39,
+        40: 59,
+        60: 79,
+        80: 99,
+        100: 119,
+        120: 139,
+        140: 159,
+        160: 179,
+        180: 199,
+        200: 219,
+        220: 239,
+        240: 259,
+        260: 279,
+        280: 299,
+        300: 319,
+        320: 339,
+        340: 359,
+        360: 379,
+        380: 399,
+    },
+    right: {
+        19: 0,
+        39: 20,
+        59: 40,
+        79: 60,
+        99: 80,
+        119: 100,
+        139: 120,
+        159: 140,
+        179: 160,
+        199: 180,
+        219: 200,
+        239: 220,
+        259: 240,
+        279: 260,
+        299: 280,
+        319: 300,
+        339: 320,
+        359: 340,
+        379: 360,
+        399: 380,
+    }
+}
+const cornerUpBoxNumberList = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
+const cornerDownBoxNumberList = [380, 381, 382, 383, 384, 385, 386, 387, 388, 389, 390, 391, 392, 393, 394, 395, 396, 397, 398, 399, 400];
+const cornerRightBoxNumberList = [19, 39, 59, 79, 99, 119, 139, 159, 179, 199, 219, 239, 259, 279, 299, 319, 339, 359, 379, 399];
+const cornerLeftBoxNumberList = [0, 20, 40, 60, 80, 100, 120, 140, 160, 180, 200, 220, 240, 260, 280, 300, 320, 340, 360, 380];
 let snakeLength = 4;
 let snakeTailMotionStack = ['rightShrink', 'rightShrink', 'rightShrink', 'rightShrink'];
 let snakeBoxIndexStack = [106, 107, 108, 109];
 let currentDirection = 'right';
 
 setInterval(() => {
+    console.log(snakeBoxIndexStack);
     moveSnake();
 }, 200);
 
@@ -17,7 +114,10 @@ function moveSnake() {
         snakeBoxes[snakeBoxIndexStack[0]].removeAttribute('class');
         snakeBoxes[snakeBoxIndexStack[0]].classList.add(snakeTailMotionStack[0]);
 
-        const nextSnakeBoxIndex = snakeBoxIndexStack[snakeLength - 1] - canvasBoxNumber;
+        let nextSnakeBoxIndex = snakeBoxIndexStack[snakeLength - 1] - canvasBoxNumber;
+        if (cornerUpBoxNumberList.includes(snakeBoxIndexStack[snakeLength - 1])) {
+            nextSnakeBoxIndex = cornerBoxMapping['up'][snakeBoxIndexStack[snakeLength - 1]];
+        }
         snakeBoxes[nextSnakeBoxIndex].removeAttribute('class');
         snakeBoxes[nextSnakeBoxIndex].classList.add('upGrow');
 
@@ -30,7 +130,10 @@ function moveSnake() {
         snakeBoxes[snakeBoxIndexStack[0]].removeAttribute('class');
         snakeBoxes[snakeBoxIndexStack[0]].classList.add(snakeTailMotionStack[0]);
 
-        const nextSnakeBoxIndex = snakeBoxIndexStack[snakeLength - 1] + canvasBoxNumber;
+        let nextSnakeBoxIndex = snakeBoxIndexStack[snakeLength - 1] + canvasBoxNumber;
+        if (cornerDownBoxNumberList.includes(snakeBoxIndexStack[snakeLength - 1])) {
+            nextSnakeBoxIndex = cornerBoxMapping['down'][snakeBoxIndexStack[snakeLength - 1]];
+        }
         snakeBoxes[nextSnakeBoxIndex].removeAttribute('class');
         snakeBoxes[nextSnakeBoxIndex].classList.add('downGrow');
 
@@ -43,7 +146,10 @@ function moveSnake() {
         snakeBoxes[snakeBoxIndexStack[0]].removeAttribute('class');
         snakeBoxes[snakeBoxIndexStack[0]].classList.add(snakeTailMotionStack[0]);
 
-        const nextSnakeBoxIndex = snakeBoxIndexStack[snakeLength - 1] - 1;
+        let nextSnakeBoxIndex = snakeBoxIndexStack[snakeLength - 1] - 1;
+        if (cornerLeftBoxNumberList.includes(snakeBoxIndexStack[snakeLength - 1])) {
+            nextSnakeBoxIndex = cornerBoxMapping['left'][snakeBoxIndexStack[snakeLength - 1]];
+        }
         snakeBoxes[nextSnakeBoxIndex].removeAttribute('class');
         snakeBoxes[nextSnakeBoxIndex].classList.add('leftGrow');
 
@@ -56,7 +162,10 @@ function moveSnake() {
         snakeBoxes[snakeBoxIndexStack[0]].removeAttribute('class');
         snakeBoxes[snakeBoxIndexStack[0]].classList.add(snakeTailMotionStack[0]);
 
-        const nextSnakeBoxIndex = snakeBoxIndexStack[snakeLength - 1] + 1;
+        let nextSnakeBoxIndex = snakeBoxIndexStack[snakeLength - 1] + 1;
+        if (cornerRightBoxNumberList.includes(snakeBoxIndexStack[snakeLength - 1])) {
+            nextSnakeBoxIndex = cornerBoxMapping['right'][snakeBoxIndexStack[snakeLength - 1]];
+        }
         snakeBoxes[nextSnakeBoxIndex].removeAttribute('class');
         snakeBoxes[nextSnakeBoxIndex].classList.add('rightGrow');
 
